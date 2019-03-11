fbe36951d5205b75748132383115e8fc0c8fcada
gab709 <m.gabriele1997@hotmail.it>
2019-03-11 19:09:55 +0100

    Aggiunta cambio scena con componenti di default

---
+++ TimeFix/Assets/Scripts/Network/PlayerConnectionObjectScript.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+//gestisce lo spawn dei 2 player
+public class PlayerConnectionObjectScript : NetworkBehaviour {
+
+    public GameObject playerUnitHost;
+    public GameObject playerUnitClient;
+    private GameObject camHost;
+    private GameObject camClient;
+    // Use this for initialization
+    void Start () {
+        camHost= playerUnitHost.transform.FindChild("MainCameraHost").gameObject;
+        camClient = playerUnitClient.transform.FindChild("MainCameraClient").gameObject;
+
+        if (isLocalPlayer == false)
+        {
+            camHost.SetActive(false);
+            camClient.SetActive(false);
+            return;
+        }
+
+
+        camClient.SetActive(true);
+        camHost.SetActive(true);
+
+
+        CmdSpawnMyUnit();
+        /*camHost.SetActive(true);
+        camClient.SetActive(true);
+
+        if (isLocalPlayer==false) {
+
+            if (DataScript.playersObject.Count == 0)
+            {
+                camHost.SetActive(false);
+            }
+            else
+            {
+                camClient.SetActive(false);
+            }
+
+            return;
+        }
+
+        camClient.SetActive(true);
+
+        CmdSpawnMyUnit();
+        */
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (isLocalPlayer == false)
+        {
+            return;
+        }
+
+    }
+
+    [Command]
+     void CmdSpawnMyUnit() {
+        if (DataScript.playersObject.Count==0)
+        {
+
+            GameObject go = Instantiate(playerUnitHost);
+            DataScript.AddToUserList(go);
+            go.transform.position = NetworkManager.singleton.GetStartPosition().position;//posizione iniziale
+            NetworkServer.SpawnWithClientAuthority(go, connectionToClient);
+        }
+        else {
+
+            GameObject go = Instantiate(playerUnitClient);
+            DataScript.AddToUserList(go);
+            go.transform.position = NetworkManager.singleton.GetStartPosition().position;//posizione iniziale
+            NetworkServer.SpawnWithClientAuthority(go, connectionToClient);
+        }
+       
+    }
+    
+    
+}
---
+++ TimeFix/Assets/Scripts/Players/LiamMovement.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LiamMovement : NetworkBehaviour
+{
+
+	private Animator animator;
+	private Rigidbody rb;
+	public float speed = 1f;
+	public float velocity = 0;
+	public float turnSpeed = 1f;
+	public float turnVelocity = 0.1f;
+	public float time = 0f;
+	public bool tmp = false;
+    public int life = 100;
+    public bool died = false;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		animator = GetComponent<Animator>();
+		rb = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+	// Update is called once per frame
+
+    void FixedUpdate()
+    {
+        if (hasAuthority == false) {
+            return;
+        }
+		float y = Input.GetAxis("Vertical");
+		float x = Input.GetAxis("Horizontal");
+		velocity = y * speed;
+
+		turnVelocity = x * turnSpeed;
+
+		animator.SetBool("Kick", false);
+		animator.SetBool("Punch", false);
+
+
+		if (velocity != 0 && ( ((Time.time - time) > 4.7) || time == 0))
+		{
+			time = 0f;
+			animator.SetBool("Take", false);
+			animator.SetBool("IsMoving", true);
+			animator.SetFloat("Velocity", y);
+			animator.SetBool("Fight", false);
+			
+			tmp = true;
+			rb.velocity = transform.forward * velocity;
+		}
+		else
+		{
+			animator.SetBool("IsMoving", false);
+			animator.SetBool("Take", false);
+			tmp = false;
+		}
+
+
+		if (Input.GetKeyDown(KeyCode.F) && tmp==false)
+		{
+			animator.SetBool("Take", true);
+			time = Time.time;
+		}
+
+        CapsuleCollider collider = this.gameObject.GetComponent<CapsuleCollider>();
+
+		if (Input.GetKeyDown(KeyCode.Space) )
+		{
+			animator.SetBool("Jump", true);
+		}
+		else
+		{
+           animator.SetBool("Jump", false);
+        }
+		
+
+		Vector3 rotationToApply = new Vector3(0, turnVelocity, 0);
+
+		rb.rotation = Quaternion.Euler(rb.rotation.eulerAngles + rotationToApply);
+
+		animator.SetFloat("TurnRate", x);
+
+        if(life <= 0)
+        {
+            animator.SetBool("Died", true);
+            died = true;
+        }
+
+        /*
+        if (died)
+        {
+            Destroy(this.gameObject);
+        }
+		*/
+
+		if (Input.GetKeyDown(KeyCode.G))
+		{
+			animator.SetBool("Fight", true);
+
+		}
+
+		if (Input.GetKeyDown(KeyCode.Q))
+		{
+			animator.SetBool("Kick", true);
+		}
+
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			animator.SetBool("Punch", true);
+		}
+	}
+}
