521eb18590f6392616114cd584485faeec5d2c5d
gab709 <m.gabriele1997@hotmail.it>
2019-03-11 18:19:26 +0100

    Aggiunta menu iniziale e camera di default in tutti i livelli

---
+++ TimeFix/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    private void OnClick()
+    {
+        SceneManager.LoadScene("Livello1", LoadSceneMode.Single);
+    }
+}
