4c1173433bc11a3a2113e2a2fc86bcaec09d87df
gab709 <m.gabriele1997@hotmail.it>
2019-03-28 17:30:27 +0100

    Aggiunta Sun Temple Base

---
+++ TimeFix/Assets/Scripts/ScriptSun/Hint1_Book.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hint1_Book : MonoBehaviour
+{
+    public GameObject allertGuiA;
+    public GameObject allertGuiB;
+    public GameObject panelGuiA;
+    public GameObject panelGuiB;
+    public GameObject allertGuiPanelA;
+    public GameObject allertGuiPanelB;
+
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("PlayerA"))
+        {
+            allertGuiA.gameObject.SetActive(true);
+            allertGuiA.gameObject.GetComponent<Text>().text = "Premi H per leggere il libro";
+            
+        }
+        if (collision.gameObject.CompareTag("PlayerB"))
+        {
+            allertGuiB.gameObject.SetActive(true);
+            allertGuiB.gameObject.GetComponent<Text>().text = "Premi H per leggere il libro";
+            
+        }
+
+
+    }
+    public void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("PlayerA"))
+        {
+
+            if (Input.GetKeyDown(KeyCode.H))
+            {
+                allertGuiA.gameObject.SetActive(false);
+                panelGuiA.gameObject.SetActive(true);
+                allertGuiPanelA.gameObject.GetComponent<Text>().text = "FIKKOOO L INdizio";
+            }
+        }
+        if (collision.gameObject.CompareTag("PlayerB"))
+        {
+
+            if (Input.GetKeyDown(KeyCode.H))
+            {
+                allertGuiB.gameObject.SetActive(false);
+                panelGuiB.gameObject.SetActive(true);
+                allertGuiPanelB.gameObject.GetComponent<Text>().text = "FIKKOOO L INdizio";
+            }
+        }
+
+
+    }
+    public void OnCollisionExit(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag("PlayerA"))
+        {
+            panelGuiA.gameObject.SetActive(false);
+            allertGuiA.gameObject.SetActive(false);
+        }
+        if (collision.gameObject.CompareTag("PlayerB"))
+        {
+            panelGuiB.gameObject.SetActive(false);
+            allertGuiB.gameObject.SetActive(false);
+        }
+
+    }
+
+}
---
+++ TimeFix/Assets/Scripts/ScriptSun/TurnOnSun.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnSun : MonoBehaviour
+{
+    public GameObject gameController;
+
+    public void OnTriggerEnter(Collider other)
+    {
+
+        gameController.GetComponent<LevelComplete>().AddPlayer();
+    
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        gameController.GetComponent<LevelComplete>().RemovePlayer();
+    }
+}
