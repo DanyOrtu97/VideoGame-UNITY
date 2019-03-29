f3873eb0c555476f3e3c798c5545cd64283cfbe4
gab709 <m.gabriele1997@hotmail.it>
2019-03-29 01:41:49 +0100

    Aggiunta controller con joypad

---
+++ TimeFix/Assets/Scripts/MenuScript/InputAssign.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputAssign : MonoBehaviour
+{
+    public static Dictionary<string, string> keyDict = new Dictionary<string, string>() {
+        { "PlayerAHorizontal","Horizontal"},
+        { "PlayerAVertical","Vertical"},
+        { "PlayerBHorizontal","HorizontalB"},
+        { "PlayerBVertical","VerticalB"}
+    };
+    public static void UpdateDictionary(string playerAx,string ax)
+    {
+        if (!keyDict.ContainsKey(playerAx))
+            keyDict.Add(playerAx, ax);
+        else
+            keyDict[playerAx] = ax;
+    }
+
+}
