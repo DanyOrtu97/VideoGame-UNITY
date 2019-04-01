bbf3f3dc05e154d75a27fae5dc10eefa0a3c20d0
gab709 <m.gabriele1997@hotmail.it>
2019-04-01 11:17:53 +0200

    Modifiche sun temple: Collisioni, timer.

---
+++ TimeFix/Assets/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public GameObject door;
+    private float timeLeft = 30.0f;
+    public GameObject textTimer;
+    // Start is called before the first frame update
+
+
+
+    void FixedUpdate()
+    {
+        timeLeft -= Time.deltaTime;
+        textTimer.gameObject.GetComponent<Text>().text = ((int)timeLeft).ToString();
+        if (timeLeft < 0) {
+            Debug.Log("Game over");
+        }
+
+    }
+}
