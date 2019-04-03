5483f074fcbd3129ae43f3c5adaca0050e17cd9b
gab709 <m.gabriele1997@hotmail.it>
2019-04-03 18:50:41 +0200

    Aggiunta canvas caricamento. Allegerimento del livello Sun Temple

---
+++ TimeFix/Assets/Scripts/Misc/ChangeSceneAsync.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChangeSceneAsync : MonoBehaviour
+{
+
+    AsyncOperation sceneAO;
+    public GameObject loadingUI;
+    public Slider loadingProgbar;
+    public Text loadingText;
+
+    // the actual percentage while scene is fully loaded
+    private const float LOAD_READY_PERCENTAGE = 0.90f;
+
+    public void ChangeScene(string sceneName)
+    {
+        loadingUI.SetActive(true);
+        loadingText.text = "0%";
+        StartCoroutine(LoadingSceneRealProgress(sceneName));
+    }
+
+    IEnumerator LoadingSceneRealProgress(string sceneName)
+    {
+        yield return new WaitForSeconds(1);
+        sceneAO = SceneManager.LoadSceneAsync(sceneName);
+
+        // disable scene activation while loading to prevent auto load
+        sceneAO.allowSceneActivation = false;
+
+        while (!sceneAO.isDone)
+        {
+            loadingProgbar.value = sceneAO.progress;
+            loadingText.text = ((int)(loadingProgbar.value * 100)+11).ToString()+"%";
+
+            if (sceneAO.progress >= LOAD_READY_PERCENTAGE)
+            {
+                loadingProgbar.value = 1f;
+                sceneAO.allowSceneActivation = true;
+                
+            }
+            Debug.Log(sceneAO.progress);
+            yield return null;
+        }
+    }
+}
+
