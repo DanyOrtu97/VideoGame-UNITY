801b8bdd1d1b6749162189098e5de131e0c748de
DanyOrtu97 <danieleortu8@gmail.com>
2019-04-08 14:18:20 +0200

    Fix menu

---
+++ TimeFix/Assets/Scripts/MenuScript/ApplicationManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ApplicationManager : MonoBehaviour {
+
+    public GameObject impostazioni;
+    public GameObject menu;
+
+	public void Quit () 
+	{
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+
+    public void GoImpostazioni()
+    {
+        menu.SetActive(false);
+        impostazioni.SetActive(true);
+    }
+
+    public void GoMenu()
+    {
+        menu.SetActive(true);
+        impostazioni.SetActive(false);
+    }
+}
