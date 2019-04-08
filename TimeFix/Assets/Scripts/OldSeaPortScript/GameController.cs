1a88875b116db5d96343c9b63d4178efe4202500
aprs3 <andrea.piras.3@gmail.com>
2019-04-09 01:03:06 +0200

    Aggiunto OldSeaPort Lvl 1

---
+++ TimeFix/Assets/Scripts/OldSeaPortScript/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private int counterElemBarca = 0;
+    private int counterFood = 0;
+
+    public int getCounterBarca(){
+        return counterElemBarca;
+
+    }
+
+    public void setCounterBarca(){
+        counterElemBarca++;
+    }
+
+    public int getCounterFood()
+    {
+        return counterFood;
+    }
+
+    public void setCounterFood()
+    {
+        counterFood++;
+    }
+
+}
