7d05c5b4b94904f3503a69903172a5d8e2c94533
gab709 <m.gabriele1997@hotmail.it>
2019-04-08 14:17:24 +0200

    Fix menu

--- TimeFix/Assets/Scripts/ScriptSun/IntroSunTemple.cs
+++ TimeFix/Assets/Scripts/ScriptSun/IntroSunTemple.cs
@@ -6,12 +6,16 @@ public class IntroSunTemple : MonoBehaviour
 {
 
     public GameObject introCanvas;
+    public GameObject[] element;
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.P)) {
             introCanvas.SetActive(false);
             this.GetComponent<Timer>().startTimer = true;
+            foreach(GameObject item in element) {
+                item.SetActive(true);
+            }
         }
     }
 }
