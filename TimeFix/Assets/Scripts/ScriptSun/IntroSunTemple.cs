288883b26b992076480e335348d21e7370535abb
gab709 <m.gabriele1997@hotmail.it>
2019-04-08 12:59:31 +0200

    Dialed text

---
+++ TimeFix/Assets/Scripts/ScriptSun/IntroSunTemple.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroSunTemple : MonoBehaviour
+{
+
+    public GameObject introCanvas;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P)) {
+            introCanvas.SetActive(false);
+            this.GetComponent<Timer>().startTimer = true;
+        }
+    }
+}
