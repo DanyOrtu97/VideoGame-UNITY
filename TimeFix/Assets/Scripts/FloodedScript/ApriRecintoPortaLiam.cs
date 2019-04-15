d7647eddab8e92187e40ac4a094c5410323792c6
nicop <nico.pisano95@gmail.com>
2019-04-15 18:25:06 +0200

    LIVELLO 3 FINITO !! QUASI ...

--- TimeFix/Assets/Scripts/FloodedScript/ApriRecintoPortaLiam.cs
+++ TimeFix/Assets/Scripts/FloodedScript/ApriRecintoPortaLiam.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ApriPortaRecintoLiam : MonoBehaviour
+public class ApriRecintoPortaLiam : MonoBehaviour
 {
     public Text textA;
     public GameObject infoA;
